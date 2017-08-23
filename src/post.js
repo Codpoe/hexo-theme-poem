a05ba67b02e85947289b04d8daf14ac54e5d0f80
Codpoe <codpoe.me@gmail.com>
2017-08-24 03:18:45 +0800

    [M]update the header and wrapper animation

--- src/post.js
+++ src/post.js
@@ -9,7 +9,13 @@ import './js/common.js';
 import draw from './js/triangle-canvas.js';
 
 var canvas = document.querySelector('.triangle-canvas');
+var wrapper = document.querySelector('.wrapper');
+
 if (canvas) {
     draw(false, canvas);
+}
+
+window.onload = function (ev) {
+    wrapper.classList.add('wrapper--show');
 }
 
