d8935d4c226874250cf2171dd98de5114c0ac8fb
Codpoe <codpoe.me@gmail.com>
2017-09-08 02:37:41 +0800

    Fix header overflow:hidden. Update header__post

--- src/js/post.js
+++ src/js/post.js
@@ -1,17 +1,19 @@
 var header = document.querySelector('.header');
+var headerNormal = document.querySelector('.header__normal');
+var headerPost = document.querySelector('.header__post-hidden');
 var lastScrollTop = 0;
 var isHeaderSwitch = false;
 
 window.addEventListener('scroll', function (ev) {
     var scrollTop = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
     var distance = scrollTop - lastScrollTop;
-    if (Math.abs(distance) < 80) {
+    if (Math.abs(distance) < 100) {
         return;
     }
-    if (!isHeaderSwitch && distance >= 80) {
+    if (!isHeaderSwitch && distance >= 100) {
         isHeaderSwitch = true;
         header.classList.toggle('header--switch');
-    } else if (isHeaderSwitch && distance <= -80) {
+    } else if (isHeaderSwitch && distance <= -100) {
         isHeaderSwitch = false;
         header.classList.toggle('header--switch');
     }
